d14809d74f0daa32412d449790dde807757330f1
Gustavo Yuken <eaiyukenish4.0@gmail.com>
2025-12-05 09:12:42 -0300

    Exercício 115 função design sistema

---
+++ Curso em vídeo - Python/Revisão exercício 115/modulos_sistemas/cadastro_design/__init__.py
@@ -0,0 +1,28 @@
+def header_conf(a=''):
+    from time import sleep
+    tam = len(a)
+    print('-'*40)
+    print(f'{'Seja bem vindo ao sistema de cadastro':^40}')
+    print('-' * 40)
+    print(f'\033[0:33mCarregando...\033[m')
+    sleep(1)
+    print('-'*40)
+    print(f'{a:^40}')
+    print('-' * 40)
+    system('Menu')
+
+
+def system(b=''):
+    if b == 'Menu':
+        print(f'\033[0:33m1\033[m - \033[0:32mVer pessoas cadastradas\033[m')
+        print(f'\033[0:33m2\033[m - \033[0:32mCadastrar nova Pessoa\033[m')
+        print(f'\033[0:33m3\033[m - \033[0:32mSair do Sistema\033[m')
+        print('-' * 40)
+        try:
+            n = int(input('Digite sua opção: '))
+        except KeyboardInterrupt:
+            print('\033[0:31mVocê desejou sair do programa\033[m')
+        except Exception as ex:
+            print(f'\033[0:31mErro: {ex}\033[m')
+        else:
+            return n
